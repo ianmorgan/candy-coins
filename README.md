11e84e5555545917fcdb4aa53e1508abb0f18331
Ian Morgan <ian.morgan@r3.com>
2018-09-08 13:26:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+![Corda](https://www.corda.net/wp-content/uploads/2016/11/fg005_corda_b.png)
+
+
+# KandyKoins 
+
+An example [CorDapp](http://www.corda.net/develop/index.html) where kids can trade sweets 
+
+In the KandyKoins world the following rules apply:
+
+* Kids get new sweets each week and on special occasions 
+* Sweets are valued as follows:
+** 1 chocolate bar = 3 gobstoppers 
+** 1 gobstopper = 5 jellybeans
+* chocolate bars can be swapped for marbles, which can be used to buy other things like toys (really 
+marbles are the cash)
+* the chocolate bar to marble exchange rate changes daily 
+* sweets can be eaten! 
---
+++ kotlin-source/src/test/kotlin/ianmorgan/kandykoins/contract/KandyBagGiveTests.kt
@@ -0,0 +1,156 @@
+package ianmorgan.kandykoins.contract
+
+import net.corda.core.contracts.*
+import net.corda.finance.*
+import net.corda.testing.contracts.DummyState
+import net.corda.testing.node.MockServices
+import net.corda.testing.node.ledger
+import ianmorgan.kandykoins.ALICE
+import ianmorgan.kandykoins.BOB
+import ianmorgan.kandykoins.DUMMY
+import ianmorgan.kandykoins.MINICORP
+import ianmorgan.kandykoins.state.IOUState
+import ianmorgan.kandykoins.state.KandyBagState
+import org.junit.*
+
+
+class KandyBagGiveTests {
+    // A pre-defined dummy command.
+    class DummyCommand : TypeOnlyCommandData()
+    private var ledgerServices = MockServices(listOf("ianmorgan.kandykoins"))
+
+
+    @Test
+    fun mustIncludeGiveCommand() {
+        val bag = KandyBagState(1,2,3,ALICE.party)
+        ledgerServices.ledger {
+            transaction {
+                output(KandyBagContract.KANDY_CONTRACT_ID,  bag)
+                command(listOf(ALICE.publicKey), DummyCommand()) // Wrong type.
+                this.fails()
+            }
+            transaction {
+                output(KandyBagContract.KANDY_CONTRACT_ID,  bag)
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give()) // Correct type.
+                this.verifies()
+            }
+        }
+    }
+
+    @Test
+    fun giveTransactionMustHaveNoInputs() {
+        val bag = KandyBagState(1,2,3,ALICE.party)
+        ledgerServices.ledger {
+            transaction {
+                input(KandyBagContract.KANDY_CONTRACT_ID, DummyState())
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                output(KandyBagContract.KANDY_CONTRACT_ID, bag)
+                this `fails with` "No inputs should be consumed when giving a Kandy Bag."
+            }
+            transaction {
+                output(KandyBagContract.KANDY_CONTRACT_ID, bag)
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                this.verifies() // As there are no input states.
+            }
+        }
+    }
+
+
+    @Test
+    fun giveTransactionMustHaveOneOutput() {
+        val bag = KandyBagState(1,2,3,ALICE.party)
+        ledgerServices.ledger {
+            transaction {
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                output(KandyBagContract.KANDY_CONTRACT_ID, bag) // Two outputs fails.
+                output(KandyBagContract.KANDY_CONTRACT_ID, bag)
+                this `fails with` "Only one output state should be created when giving a Kandy Bag."
+            }
+            transaction {
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                output(KandyBagContract.KANDY_CONTRACT_ID, bag)
+                this.verifies()
+            }
+        }
+    }
+
+
+    @Test
+    fun cannotGiveAnEmptyBag() {
+        ledgerServices.ledger {
+            transaction {
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                output(KandyBagContract.KANDY_CONTRACT_ID, KandyBagState(0,0,0, ALICE.party)) // Zero amount fails.
+                this `fails with` "A newly gifted bag must have kandy."
+            }
+            transaction {
+                command(listOf(ALICE.publicKey), KandyBagContract.Commands.Give())
+                output(KandyBagContract.KANDY_CONTRACT_ID, KandyBagState(1,0,0, ALICE.party))
+                this.verifies()
+            }
+        }
+    }
+
+
+
+    /**
+     * Task 6.
+     * The list of public keys which the commands hold should contain all of the participants defined in the [IOUState].
+     * This is because the IOU is a bilateral agreement where both parties involved are required to sign to issue an
+     * IOU or change the properties of an existing IOU.
+     * TODO: Add a contract constraint to check that all the required signers are [IOUState] participants.
+     * Hint:
+     * - In Kotlin you can perform a set equality check of two sets with the == operator.
+     * - We need to check that the signers for the transaction are a subset of the participants list.
+     * - We don't want any additional public keys not listed in the IOUs participants list.
+     * - You will need a reference to the Issue command to get access to the list of signers.
+     * - [requireSingleCommand] returns the single required command - you can assign the return value to a constant.
+     *
+     * Kotlin Hints
+     * Kotlin provides a map function for easy conversion of a [Collection] using map
+     * - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/map.html
+     * [Collection] can be turned into a set using toSet()
+     * - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/to-set.html
+     */
+//    @Test
+//    fun lenderAndBorrowerMustSignIssueTransaction() {
+//        val bag = KandyBagState(1,2,3,ALICE.party)
+//        ledgerServices.ledger {
+//            transaction {
+//                command(DUMMY.publicKey, IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this `fails with` "Both lender and borrower together only may sign IOU issue transaction."
+//            }
+//            transaction {
+//                command(ALICE.publicKey, IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this `fails with` "Both lender and borrower together only may sign IOU issue transaction."
+//            }
+//            transaction {
+//                command(BOB.publicKey, IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this `fails with` "Both lender and borrower together only may sign IOU issue transaction."
+//            }
+//            transaction {
+//                command(listOf(BOB.publicKey, BOB.publicKey, BOB.publicKey), IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this `fails with` "Both lender and borrower together only may sign IOU issue transaction."
+//            }
+//            transaction {
+//                command(listOf(BOB.publicKey, BOB.publicKey, MINICORP.publicKey, ALICE.publicKey), IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this `fails with` "Both lender and borrower together only may sign IOU issue transaction."
+//            }
+//            transaction {
+//                command(listOf(BOB.publicKey, BOB.publicKey, BOB.publicKey, ALICE.publicKey), IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this.verifies()
+//            }
+//            transaction {
+//                command(listOf(ALICE.publicKey, BOB.publicKey), IOUContract.Commands.Issue())
+//                output(IOUContract.IOU_CONTRACT_ID, iou)
+//                this.verifies()
+//            }
+//        }
+//    }
+}
